4b78da914cade8931ebbe0fd18e5668c8e7e9ffb
Horacio Correia <rmcorre@gmail.com>
2021-06-05 02:28:24 +0000

    Add regular Bootstrap V5. Modify Login to use Bootstrap classes instead of MDBReact. Add Bootstrap and Sass packages.

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 import AdminNavbar from '../components/UI/Navbar/AdminNavbar';
@@ -7,16 +7,26 @@ import Login from '../components/Login/Login';
 import styles from './Admin.module.css';
 
 const Admin = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  localStorage.setItem('isLoggedIn', '0');
+
+  useEffect(() => {
+    if (localStorage.getItem('isLoggedIn') === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <>
-      <AdminNavbar />
+      {isLoggedIn && <AdminNavbar />}
       <MDBContainer size="md">
         <MDBRow>
           <header></header>
         </MDBRow>
-        <MDBRow className="min-vh-100">
+        <MDBRow className={`${styles.wrapper}`}>
           <MDBCol className="col-5 mx-auto">
-            <Login></Login>
+            <Login />
           </MDBCol>
         </MDBRow>
       </MDBContainer>
