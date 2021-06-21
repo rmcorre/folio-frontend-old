0e4c25b3bc791bc9b520b86a2eec1c5ec95de32a
Horacio Correia <rmcorre@gmail.com>
2021-06-21 20:36:09 +0000

    Resolve merge conflict.

--- src/toPDF.js
+++ src/toPDF.js
@@ -1,7 +1,6 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
-
   //Launch a new browser session
   const browser = await puppeteer.launch();
 
@@ -9,20 +8,30 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
 
   //Go to our page we serve on localhost;3000
-  await page.goto("http://localhost:3000/resume", {waitUntil: 'networkidle0', timeout: 0});
+  await page.goto('http://localhost:3000/resume');
 
-  await page.emulateMediaType('screen');
+  // await page.emulateMediaType('screen');
 
   //Store the pdf in a file named resume.pdf
-  await page.pdf({path: "resume2.pdf", printBackground: true, width: '1366px', height: '1296px'});
+  await page.pdf({
+    path: 'resume2.pdf',
+    format: 'letter',
+  });
 
   await browser.close();
 })();
 
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('http://localhost:3000/resume');
-//   await page.screenshot({path: 'screenshot2.png', fullPage: true});
-//   await browser.close();
-// })();
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/resume');
+  await browser.close();
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/resume');
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
+})();
